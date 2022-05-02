ce05c3cb87195cfb0884209261b4e456c9b379e0
manuelrojasm <92123613+manuelrojasm@users.noreply.github.com>
2022-05-02 10:04:28 -0500

    frist commit

---
+++ ETITC_API_EquipmentControl/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace equipmentControl
+{
+    public class Constants
+    {
+        public static string DataBase = "RegisterDevices";
+
+        public static string Client = "mongodb+srv://krmapun:5rB1BSVS0h0xjtBV@cluster0.ioxde.mongodb.net/Cluster0?retryWrites=true&w=majority";
+    }
+}
---
+++ ETITC_API_EquipmentControl/Context/MongoDBRepository.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace equipmentControl.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient client; 
+        public IMongoDatabase db;
+
+        public MongoDBRepository()
+        {   
+            client = new MongoClient(Constants.Client);
+            db = client.GetDatabase(Constants.DataBase);
+
+        }
+    }
+}
